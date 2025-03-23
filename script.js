a068b1240501296e2477738cb38210716c9cbecd
Nivaldo Ferrari Junior <nivaldoferrari79@gmail.com>
2025-03-23 20:53:51 -0300

    Fix backbound and sort

--- script.js
+++ script.js
@@ -33,7 +33,11 @@ function ordenarNumeros(array) {
 }
 
 function randomizarNumeros(array){
-   return array.sort(()=> Math.random()-0.5)
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
 }
 
 randomizarNumeros(numerosBingo);
